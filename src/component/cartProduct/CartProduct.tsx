c42ecdd9885665790980ce14cf085b64da496e79
Nicoogf <nicolasgfalabella@gmail.com>
2023-10-04 09:46:16 -0300

    Dispatch git reset

--- src/component/cartProduct/CartProduct.tsx
+++ src/component/cartProduct/CartProduct.tsx
@@ -1,6 +1,23 @@
+import { useContext } from "react";
 import styles from "./CartProduct.module.css" ;
+import { CartContext } from "../../context/CartContext";
 
 const CartProduct = ( {product} ) => {
+
+  const { dispatch } = useContext( CartContext ) ;
+
+  const item = {
+    id: product.tail,
+    name: product.name,
+    image: product.image,
+    quantity: 1
+  }
+
+  const addToCart = ( item ) => {
+    dispatch ( {type: "ADD_TO_CART" , payload: item } )
+  }
+
+
   return (
     <div className= { styles.cardContainer } >
         <img src= { product.image }  alt= { product.name } className= { styles.cardImage } width={95} height={95}/>       
@@ -10,7 +27,7 @@ const CartProduct = ( {product} ) => {
             <p className= { styles.cardType }>  { product.type } </p>
             <p className= { styles.cardPrice }>  { product.price  } <small> 00 </small>   </p>
           </div>
-          <button className= { styles.cardButton }> Agregar Carrito</button>
+          <button className= { styles.cardButton } onClick = { ()=>addToCart(item)}> Agregar Carrito</button>
       </div>
     </div>
   ) ;
